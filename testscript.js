2f193a97dc2081c2403187f1a1d6b5f91c344868
rhynep <64207371+rhynep@users.noreply.github.com>
2020-05-07 21:03:26 -0400

    javascript images

--- testscript.js
+++ testscript.js
@@ -81,81 +81,81 @@ function expandImage() {
 }
 
 // the hierophant
-theImgDiv = document.getElementById("card1");
-theImage = document.getElementById("cardimage1");
-theImgDiv.addEventListener("click", expandImage1);
-theImage = document.getElementById("cardimage1");
+theImgDiv1 = document.getElementById("card1");
+theImage1 = document.getElementById("cardimage1");
+theImgDiv1.addEventListener("click", expandImage1);
+theImage1 = document.getElementById("cardimage1");
 
 function expandImage1() {
-    if (theImgDiv.style.position == "") {
-        theImgDiv.style.position = "absolute";
-        theImgDiv.style.top = "100";
-        theImgDiv.style.left = "100";
-        theImage.style.width = "auto";
-        theImage.src = "https://rhynep.github.io/CTIS230Assignments/testing/image017.png";
+    if (theImgDiv1.style.position == "") {
+        theImgDiv1.style.position = "absolute";
+        theImgDiv1.style.top = "100";
+        theImgDiv1.style.left = "100";
+        theImage1.style.width = "auto";
+        theImage1.src = "https://rhynep.github.io/CTIS230Assignments/testing/image001.png";
     } 
     else { // makes it small
-        theImgDiv.style.position = "";
-        theImage.style.width = "";
+        theImgDiv1.style.position = "";
+        theImage1.style.width = "";
     }
 }
 
 // the chariot
-theImgDiv = document.getElementById("card2");
-theImage = document.getElementById("cardimage2");
-theImgDiv.addEventListener("click", expandImage2);
-theImage = document.getElementById("cardimage2");
+theImgDiv2 = document.getElementById("card2");
+theImage2 = document.getElementById("cardimage2");
+theImgDiv2.addEventListener("click", expandImage2);
+theImage2 = document.getElementById("cardimage2");
 
 function expandImage2() {
-    if (theImgDiv.style.position == "") {
-        theImgDiv.style.position = "absolute";
-        theImgDiv.style.top = "100";
-        theImgDiv.style.left = "100";
-        theImage.style.width = "auto";
-        theImage.src = "https://rhynep.github.io/CTIS230Assignments/testing/image017.png";
+    if (theImgDiv2.style.position == "") {
+        theImgDiv2.style.position = "absolute";
+        theImgDiv2.style.top = "100";
+        theImgDiv2.style.left = "100";
+        theImage2.style.width = "auto";
+        theImage2.src = "https://rhynep.github.io/CTIS230Assignments/testing/image005.png";
     } 
     else { // makes it small
-        theImgDiv.style.position = "";
-        theImage.style.width = "";
+        theImgDiv2.style.position = "";
+        theImage2.style.width = "";
     }
 }
 
 // the moon
-theImgDiv = document.getElementById("card3");
-theImage = document.getElementById("cardimage3");
-theImgDiv.addEventListener("click", expandImage);
-theImage = document.getElementById("cardimage3");
+theImgDiv3 = document.getElementById("card3");
+theImage3 = document.getElementById("cardimage3");
+theImgDiv3.addEventListener("click", expandImage3);
+theImage3 = document.getElementById("cardimage3");
 
-function expandImage() {
-    if (theImgDiv.style.position == "") {
-        theImgDiv.style.position = "absolute";
-        theImgDiv.style.top = "100";
-        theImgDiv.style.left = "100";
-        theImage.style.width = "auto";
-        theImage.src = "https://rhynep.github.io/CTIS230Assignments/testing/image017.png";
+function expandImage3() {
+    if (theImgDiv3.style.position == "") {
+        theImgDiv3.style.position = "absolute";
+        theImgDiv3.style.top = "100";
+        theImgDiv3.style.left = "100";
+        theImage3.style.width = "auto";
+        theImage3.src = "https://rhynep.github.io/CTIS230Assignments/testing/image007.png";
     } 
     else { // makes it small
-        theImgDiv.style.position = "";
-        theImage.style.width = "";
+        theImgDiv3.style.position = "";
+        theImage3.style.width = "";
     }
 }
 
 
-theImgDiv = document.getElementById("card4");
-theImage = document.getElementById("cardimage4");
-theImgDiv.addEventListener("click", expandImage);
-theImage = document.getElementById("cardimage4");
+theImgDiv4 = document.getElementById("card4");
+theImage4 = document.getElementById("cardimage4");
+theImgDiv4.addEventListener("click", expandImage4);
+theImage4 = document.getElementById("cardimage4");
 
-function expandImage() {
-    if (theImgDiv.style.position == "") {
-        theImgDiv.style.position = "absolute";
-        theImgDiv.style.top = "100";
-        theImgDiv.style.left = "100";
-        theImage.style.width = "auto";
-        theImage.src = "https://rhynep.github.io/CTIS230Assignments/testing/image017.png";
+function expandImage4() {
+    if (theImgDiv4.style.position == "") {
+        theImgDiv4.style.position = "absolute";
+        theImgDiv4.style.top = "100";
+        theImgDiv4.style.left = "100";
+        theImage4.style.width = "auto";
+        theImage4.src = "https://rhynep.github.io/CTIS230Assignments/testing/image018.png";
     } 
     else { // makes it small
-        theImgDiv.style.position = "";
-        theImage.style.width = "";
+        theImgDiv4.style.position = "";
+        theImage4.style.width = "";
     }
 }
